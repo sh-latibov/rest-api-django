d229a3fd7736e597a98d4cfc2c8d88b29b721d46
Shamshir Latibov <sh.latibov@gmail.com>
2023-02-07 10:20:36 +0500

    check

---
+++ rest_api_site/cars/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
---
+++ rest_api_site/cars/views.py
@@ -0,0 +1,4 @@
+from rest_framework import generics
+from django.shortcuts import render
+
+class CarsAPIView()
